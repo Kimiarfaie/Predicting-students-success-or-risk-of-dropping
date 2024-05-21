7fa69605452da67b631b256370797d4f33c431b9
Kimia Arfaie <kimiaarfaie@Kimias-MacBook-Pro.local>
2024-05-21 18:17:37 +0200

    Project code

--- Project.R
+++ Project.R
@@ -1,8 +1,8 @@
 library(readr)
 
 # Read the dataset
-data <- read_csv("dataset.csv")
+data <- read_csv("/Users/kimiaarfaie/Desktop/COSI/Semester 2/Data Sceince/DAta-Science-Project/dataset.csv",show_col_types = FALSE)
 
 # Display the structure and summary of the dataset
-glimpse(data)
-summary(data)
+head(data)
+spec(data)
