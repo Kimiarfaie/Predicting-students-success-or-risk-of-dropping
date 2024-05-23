379d70f58eaee5d7b4ffd134f77e8b8867cfd119
Kimia Arfaie <kimiaarfaie@Kimias-MacBook-Pro.local>
2024-05-23 21:17:05 +0200

    Final

--- Project.R
+++
@@ -1,8 +0,0 @@
-library(readr)
-
-# Read the dataset
-data <- read_csv("/Users/kimiaarfaie/Desktop/COSI/Semester 2/Data Sceince/DAta-Science-Project/dataset.csv",show_col_types = FALSE)
-
-# Display the structure and summary of the dataset
-head(data)
-spec(data)
